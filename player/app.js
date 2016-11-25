0044db2887d5875fafa2a54b76b876b554de6484
amaloma <amaloma@gmail.com>
2016-11-25 08:17:09 +0000

    ok

--- player/app.js
+++ player/app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+var viewer = require('./routes/viewer');
+app.use('/viewer', viewer);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -55,6 +58,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
