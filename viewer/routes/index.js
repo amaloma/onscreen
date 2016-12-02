7e94863db29015d4069437baec7f26a7ed3179d1
amaloma <amaloma@gmail.com>
2016-12-02 10:31:40 +0000

    index

--- viewer/routes/index.js
+++ viewer/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ONScreen'
+    , default_duration: 10
+    , use_24_hour_clock: 1 
+  });
 });
 
 module.exports = router;
