ea98f7316fc2cd996844730b06188405a4957f66
amaloma <amaloma@gmail.com>
2016-12-13 17:11:25 +0700

    setting menu,ip_setting

--- viewer/routes/index.js
+++ viewer/routes/index.js
@@ -3,6 +3,49 @@ var router = express.Router();
 var fs = require('fs');
 const os = require('os');
 
+router.post('/ip_settings', function(req, res, next) { 
+  console.log("req.body ->",req.body)
+  // if(req.body.use_24_hour_clock == 'on') conf.use_24_hour_clock = true;
+  // else conf.use_24_hour_clock = false;
+  // if(req.body.show_splash == 'on') conf.show_splash = true;
+  // else conf.show_splash = false;
+  // conf.splash_page_delay = req.body.splash_page_delay;
+  // if(req.body.shuffle_playlist == 'on') conf.shuffle_playlist = true;
+  // else conf.shuffle_playlist = false;
+  // if(req.body.debug_logging == 'on') conf.debug_logging = true;
+  // else conf.debug_logging = false;
+  // conf.audio_output = req.body.audio_output;
+  // conf.default_duration = req.body.default_duration;
+  // conf.title = 'ONScreen'
+  var exec = require('child_process').exec; 
+  exec("sudo reboot", function(error, stdout, stderr) {
+    console.log("error ->",error)
+    // res.render('ip_settings', conf ); 
+  });
+});
+
+router.get('/ip_settings', function(req, res, next) {
+  var exec = require('child_process').exec;
+  var conf = {}
+  // console.log("conf ->",conf)
+  conf.title = 'ONScreen'
+  exec("cat /etc/dhcpcd.conf | grep '#static ip_address'", function(error, stdout, stderr) {
+    // console.log("stdout.length ->",stdout.length,stdout)
+    if(stdout.length > 1) conf.dhcp_client = true 
+    else conf.dhcp_client = false
+    exec("ip a | grep 'global eth0' | tr -s ' ' '\t' | cut -f3", function(error, stdout, stderr) {
+      conf.now_ip = stdout
+      exec("ip route | grep default | tr -s ' ' '\t' | cut -f3", function(error, stdout, stderr) {
+        conf.routers = stdout
+        exec("cat /etc/resolv.conf | grep nameserver |  tr -s ' ' '\t' | cut -f2", function(error, stdout, stderr) {
+          conf.dns = stdout
+          res.render('ip_settings', conf );   
+        });
+      });
+    });
+  });
+});
+
 router.get('/system_info', function(req, res, next) { 
   var loadavg = os.loadavg()
   var uptime = os.uptime()
@@ -36,6 +79,7 @@ router.post('/settings', function(req, res, next) {
   else conf.use_24_hour_clock = false;
   if(req.body.show_splash == 'on') conf.show_splash = true;
   else conf.show_splash = false;
+  conf.splash_page_delay = req.body.splash_page_delay;
   if(req.body.shuffle_playlist == 'on') conf.shuffle_playlist = true;
   else conf.shuffle_playlist = false;
   if(req.body.debug_logging == 'on') conf.debug_logging = true;
